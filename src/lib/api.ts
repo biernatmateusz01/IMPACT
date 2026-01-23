e7ec6c8acf2d6b66e75f2f21b6252bcb1773aaf1
biernatmateusz01 <biernat521@gmail.com>
2026-01-23 21:58:11 +0100

    headers

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -6,6 +6,12 @@ export async function getAllCategories(): Promise<string[]> {
   try {
     const res = await fetch(`${BASE_URL}/products/categories`, {
       next: { revalidate: 60 },
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+        Accept: "application/json, text/plain, */*",
+        "Accept-Language": "pl-PL,pl;q=0.9,en;q=0.8",
+      },
     });
 
     if (!res.ok) {
